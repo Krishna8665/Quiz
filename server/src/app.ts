889c998e664bd83183d69f32ab1adaec75d24059
Krishna8665 <khatrikrissna11@gmail.com>
2025-09-19 17:49:46 +0545

    chnges

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,5 @@
-import express from 'express'
-
+import express from "express";
+import bodyParser from "body-parser";
 import authRoutes from "./routes/auth";
 import quizRoutes from "./routes/quiz";
 import playerRoutes from "./routes/player";
@@ -13,5 +13,4 @@ app.use("/api/auth", authRoutes);
 app.use("/api/quiz", quizRoutes);
 app.use("/api/player", playerRoutes);
 
-
-export default app
+export default app;
