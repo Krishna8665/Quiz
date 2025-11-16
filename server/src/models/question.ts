7305b06cded9c0abe42ef2a57ec5f508f74ab212
Krishna8665 <khatrikrissna11@gmail.com>
2025-11-16 23:31:18 +0545

    question

--- server/src/models/question.ts
+++ server/src/models/question.ts
@@ -9,7 +9,7 @@ export interface IQuestion extends Document {
   text: string;
   options?: IOption[]; // optional for MCQ
   shortAnswer?: IOption; // NEW: short answer as object with _id
-  correctAnswer: string; // store _id of correct option or shortAnswer
+  correctAnswer: string; 
   category:
     | "Physics"
     | "Maths"
