e88608bd847fce460e5d577a62b10e56da4f6e56
Krishna8665 <khatrikrissna11@gmail.com>
2025-11-20 23:17:40 +0545

    quiz changes

---
+++ server/src/routes/playQuiz.ts
@@ -0,0 +1,13 @@
+// routes/quizRoutes.ts
+import { Router } from "express";
+import { startQuiz, endQuiz } from "../controller/playQuizController";
+import  { authMiddleware } from "../middleware/auth"; // Assuming you have auth middleware
+
+const router = Router();
+
+
+router.post("/start", authMiddleware, startQuiz);
+
+router.post("/end", authMiddleware, endQuiz);
+
+export default router;
