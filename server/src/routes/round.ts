9fc07e8bf04cc98e33850ba7274f55fc38df5d9d
Krishna8665 <khatrikrissna11@gmail.com>
2026-01-08 23:58:58 +0545

    aaa

--- server/src/routes/round.ts
+++ server/src/routes/round.ts
@@ -1,11 +1,4 @@
 import { Router } from "express";
-import {
-  addTeam,
-  getTeams,
-  deleteTeam,
-  addPoints,
-  reducePoints,
-} from "../controller/teamController";
 import { authMiddleware } from "../middleware/auth";
 import {
   createRound,
@@ -16,9 +9,9 @@ import {
 
 const router = Router();
 
-router.post("/create-rounds", authMiddleware(["admin"]), createRound); 
-router.get("/get-rounds", authMiddleware(["admin"]), getRounds); 
-router.get("/get-roundById", authMiddleware(["admin"]), getRoundById); 
+router.post("/create-rounds", authMiddleware(["admin"]), createRound);
+router.get("/get-rounds", authMiddleware(["admin"]), getRounds);
+router.get("/get-roundById", authMiddleware(["admin"]), getRoundById);
 router.delete("/del-rounds/:id", authMiddleware(["admin"]), deleteRound);
 
 export default router;
